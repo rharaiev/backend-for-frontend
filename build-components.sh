0a701c873494a3eeaf99f52c97c8ea29886c1073
Roman Haraiev <rharaiev@sonatype.com>
2020-12-06 11:44:26 +0200

    Added gateway

--- build-components.sh
+++ build-components.sh
@@ -18,9 +18,13 @@ mvn -f client-app/pom.xml package
 mvn -f frontend/pom.xml clean
 mvn -f frontend/pom.xml package
 
+mvn -f gateway/pom.xml clean
+mvn -f gateway/pom.xml package
+
 docker build -t lohika/bff-books-service:1.0 -f books/Dockerfile books
 docker build -t lohika/bff-authors-service:1.0 -f authors/Dockerfile authors
 docker build -t lohika/bff-web-sockets-service:1.0 -f web-sockets/Dockerfile web-sockets
 docker build -t lohika/bff-mobile-app:1.0 -f mobile-app/Dockerfile mobile-app
 docker build -t lohika/bff-client-app:1.0 -f client-app/Dockerfile client-app
 docker build -t lohika/bff-frontend:1.0 -f frontend/Dockerfile frontend
+docker build -t lohika/bff-gateway:1.0 -f gateway/Dockerfile gateway
---
+++ gateway/src/main/java/com/course/bff/gateway/config/Config.java
@@ -0,0 +1,35 @@
+package com.course.bff.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Value("${author.url")
+    private String authorUrl;
+
+    @Value("${book.url")
+    private String bookUrl;
+
+    @Value("${frontend.url")
+    private String frontendUrl;
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/v1/authors")
+                        .uri(authorUrl))
+                .route(p -> p
+                        .path("/api/v1/books")
+                        .uri(bookUrl))
+                .route(p -> p
+                        .path("/api/v1/details")
+                        .uri(frontendUrl))
+                .build();
+    }
+}
